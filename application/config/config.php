f193720a2e9c9a0d9589c8b5b5217955e4191280
Rizuki Ryumada <rizki3366@msn.cn>
2020-06-15 19:43:52 +0700

    add web.config file for IIS 8.5

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://192.168.23.51/hc_system/'; //live mode
-$config['base_url'] = 'http://localhost/hc_system'; //development mode
+$config['base_url'] = 'http://localhost/centratama_hcportal'; //development mode
 
 /*
 |--------------------------------------------------------------------------
